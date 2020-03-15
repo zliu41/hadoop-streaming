ee5566db811436af5c96f846f9fc8264d35f1e1d
Ziyang Liu <unsafeFixIO@gmail.com>
2020-03-14 22:32:36 -0700

    Some updates

--- src/HadoopStreaming.hs
+++ src/HadoopStreaming.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 
 module HadoopStreaming
   ( Mapper(..)
   , runMapper
-  , runMapper'
-  , runMapperIO
-  , runMapperIO'
+  , println
+  , incCounter
+  , incCounter'
   ) where
 
 import           Control.Exception.Extra (handle_)
 import           Control.Monad.Extra
 import           Control.Monad.IO.Class
 import           Data.Text (Text)
+import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import           Pipes (Consumer', Pipe, Producer', runEffect, (>->))
 import qualified Pipes
 import qualified Pipes.Prelude as Pipes
 import           System.Exit (die)
-import           System.IO (isEOF)
+import qualified System.IO as IO
 
 data Mapper m = forall input k v. Mapper
   (Text -> input)
@@ -29,24 +31,33 @@ data Mapper m = forall input k v. Mapper
   -- ^ A 'Pipe' transforming @input@ into @(k, v)@ pairs.
 
 runMapper :: MonadIO m => Mapper m -> m ()
-runMapper = runMapper' (pure ()) (pure ())
-
-runMapper' :: MonadIO m => m () -> m () -> Mapper m -> m ()
-runMapper' before after (Mapper dec enc trans) = do
-  before
-  runEffect $ stdin >-> Pipes.map dec >-> trans >-> Pipes.map enc >-> stdout
-  after
-
-runMapperIO :: String -> Mapper IO -> IO ()
-runMapperIO task = runMapperIO' task (pure ()) (pure ())
-
-runMapperIO' :: String -> IO () -> IO () -> Mapper IO -> IO ()
-runMapperIO' task before after = handle_ handler . runMapper' before after
-  where
-    handler e = die $ "Map task " ++ task ++ " failed: " ++ show e
+runMapper (Mapper dec enc trans) = runEffect $
+  stdin >-> Pipes.map dec >-> trans >-> Pipes.map enc >-> stdout
 
 stdin :: MonadIO m => Producer' Text m ()
-stdin = unlessM (liftIO isEOF) (liftIO Text.getLine >>= Pipes.yield >> stdin)
+stdin = unlessM (liftIO IO.isEOF) (liftIO Text.getLine >>= Pipes.yield >> stdin)
 
 stdout :: MonadIO m => Consumer' Text m r
 stdout = Pipes.for Pipes.cat (liftIO . Text.putStrLn)
+
+-- | Like 'Text.putStrLn', but writes to 'stderr'.
+println :: MonadIO m => Text -> m ()
+println = liftIO . Text.hPutStrLn IO.stderr
+
+-- | Increment a counter by 1.
+incCounter
+  :: MonadIO m
+  => Text -- ^ Group name. Must not contain comma.
+  -> Text -- ^ Counter name. Must not contain comma.
+  -> m ()
+incCounter group name = incCounter' group name 1
+
+-- | Increment a counter by @n@.
+incCounter'
+  :: MonadIO m
+  => Text -- ^ Group name. Must not contain comma.
+  -> Text -- ^ Counter name. Must not contain comma.
+  -> Int
+  -> m ()
+incCounter' group name n = println $
+  Text.intercalate "," [group, name, Text.pack (show n)]
